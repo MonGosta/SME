3c277587ddcb90be406b1dc93fac3842f8f953f5
Jose Luis Lucas Palaci <jolucpal@gmail.com>
2017-04-04 10:57:12 +0200

    Reformat Code Php Storm

--- src/repository/EventRepository.php
+++ src/repository/EventRepository.php
@@ -1,28 +1,27 @@
-<?php 
+<?php
+
 namespace Mongosta\Repository;
+
 use \PDO;
 use Mongosta\Bootstrap\Database as db;
 use Mongosta\EventModel\EventModel as em;
 
 
+class EventRepository
+{
 
+    static public function getAll()
+    {
+        $db = Db::getInstance();
+        $req = $db->query('SELECT * FROM sme_eventos');
 
-class EventRepository{
-
-	static public function getAll(){
-		$db = Db::getInstance();
-	    $req = $db->query('SELECT * FROM sme_eventos');
-
-	    foreach ($req as $evento) {
-	    	$eventos[] = new em();
-	    }
-	    //$nombres = $req;
-		return $eventos;
+        foreach ($req as $evento) {
+            $eventos[] = new em();
+        }
+        //$nombres = $req;
+        return $eventos;
     }
 
-
-	
-	
 
 }
 
