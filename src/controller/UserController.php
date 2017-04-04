b8345355d96de0888413c2b709452b9b1d0512b9
Jose Luis Lucas Palaci <jolucpal@gmail.com>
2017-04-04 10:54:55 +0200

    Reformat Code Php Storm

--- src/controller/UserController.php
+++ src/controller/UserController.php
@@ -1,66 +1,71 @@
 <?php
+
 namespace Mongosta\Controller;
 
 use Mongosta\Repository\UserRepository as Repo;
-use Mongosta\Bootstrap\View ;
+use Mongosta\Bootstrap\View;
 use Mongosta\Model\UserModel as User;
 
 class UserController
 {
-	function index(){
+    function index()
+    {
 
-		//Repo::delete(6);
-		/*$nombres = Repo::getAll();
-		$view = new View("src/views/user");
-		$view->render('index.php', ['nombres' => $nombres]);*/
+        //Repo::delete(6);
+        /*$nombres = Repo::getAll();
+        $view = new View("src/views/user");
+        $view->render('index.php', ['nombres' => $nombres]);*/
 
-		/*$usuario = Repo::findById(3);
-		$view = new View("src/views/user");
-		$view->render('index.php', ['user' => $usuario]);
-		*/
+        /*$usuario = Repo::findById(3);
+        $view = new View("src/views/user");
+        $view->render('index.php', ['user' => $usuario]);
+        */
 
-	}
+    }
 
-	function show(){
-		$user = Repo::findByEmail(1);
-		var_dump($user);
-	}
-
-
-	function register(){
-
-		if(isset($_POST['nombre'])){
-		  $user = new User ($_POST['nombre'],
-							$_POST['usuario'],
-							$_POST['contrasena'],
-							$_POST['email'],
-							$_POST['telefono'],
-							$_POST['dni']
-							);
-		    $user->save(); 	
-		}else{
-			$user = new User();
-		}
-	  	$view = new View("src/views/user");
-		$view->render('register.php', ['user' => $user]);
+    function show()
+    {
+        $user = Repo::findByEmail(1);
+        var_dump($user);
+    }
 
 
-	}
+    function register()
+    {
 
-	function delete(){
+        if (isset($_POST['nombre'])) {
+            $user = new User ($_POST['nombre'],
+                $_POST['usuario'],
+                $_POST['contrasena'],
+                $_POST['email'],
+                $_POST['telefono'],
+                $_POST['dni']
+            );
+            $user->save();
+        } else {
+            $user = new User();
+        }
+        $view = new View("src/views/user");
+        $view->render('register.php', ['user' => $user]);
 
-		if(isset($_POST['email'])){
-			$email = $_POST['email'];
-			var_dump($email);
-			$user = new User();
-			//var_dump($user);
-			$user = Repo::findByEmail($email);
-			$user->delete();
-	    }else{
-			$user = new User();
-		}
-	    $view = new View("src/views/user");
-		$view->render('delete.php', ['user' => $user]);
+
+    }
+
+    function delete()
+    {
+
+        if (isset($_POST['email'])) {
+            $email = $_POST['email'];
+            var_dump($email);
+            $user = new User();
+            //var_dump($user);
+            $user = Repo::findByEmail($email);
+            $user->delete();
+        } else {
+            $user = new User();
+        }
+        $view = new View("src/views/user");
+        $view->render('delete.php', ['user' => $user]);
     }
 }
 
