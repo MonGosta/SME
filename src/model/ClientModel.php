38d32e48a39c2b8ab777a2756324ef11bb99130a
Jose Luis Lucas Palaci <jolucpal@gmail.com>
2017-04-04 10:55:20 +0200

    REformat Code Php Storm

--- src/model/ClientModel.php
+++ src/model/ClientModel.php
@@ -1,46 +1,62 @@
 <?php
 
 namespace Mongosta\Model;
-    
-class ClientModel {
-  private $id;
-  private $nombre;
-  private $email;
-  private $telefono;
 
-  public function __construct($id=null, $nombre="", $email="", $telefono=""){
-    $this->id = $id;
-    $this->nombre = $nombre;
-    $this->email = $email;
-    $this->telefono = $telefono;
-  }
-        
-  public function getId(){
-    return $this->id;
-  }
-  public function setId($id){
-    $this->id = $id;
-  }
+use Mongosta\Repository\ClientRepository as ClientRepo;
 
-  public function getNombre(){
-    return $this->nombre;
-  }
-  public function setNombre($nombre){
-    $this->nombre = $nombre;
-  }
+class ClientModel
+{
+    private $id;
+    private $nombre;
+    private $email;
+    private $telefono;
 
-  public function getEmail(){
-    return $this->email;
-  }
-  public function setEmail($email){
-    $this->email = $email;
-  }
+    public function __construct($id = null, $nombre = "", $email = "", $telefono = "")
+    {
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->email = $email;
+        $this->telefono = $telefono;
+    }
 
-  public function getTelefono(){
-    return $this->telefono;
-  }
-  public function setTelefono($telefono){
-    $this->telefono = $telefono;
-  }
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+    public function setTelefono($telefono)
+    {
+        $this->telefono = $telefono;
+    }
 
 }
