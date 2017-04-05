9ed369840ccb39509ed4c208b127af6464f13a69
Jose Luis Lucas Palaci <jolucpal@gmail.com>
2017-04-05 09:15:37 +0200

    Vistas Controlador de eventos.

---
+++ src/views/event/delete.php
@@ -0,0 +1,10 @@
+<form method="POST">
+    <label for="descripcion">Introduce el Nombre_sms a eliminar</label>
+    <br>
+    <br>
+    <label for="nombre">Nombre_sms: </label>
+    <input type="" name="nombre_sms" value="<?= $event->getNombre_sms(); ?>">
+    <br>
+    <br>
+    <input type="submit">
+</form>
