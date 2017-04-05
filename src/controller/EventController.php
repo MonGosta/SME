fedf9e16d72651942caef8dbd28f98314b65d293
Jose Luis Lucas Palaci <jolucpal@gmail.com>
2017-04-05 09:14:48 +0200

    Controlador de Eventos

---
+++ src/controller/EventController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Mongosta\Controller;
+
+use Mongosta\Bootstrap\View;
+use Mongosta\Model\EventModel as Event;
+use Mongosta\Repository\EventRepository as Repo;
+
+class EventController
+{
+    function index()
+    {
+        $nombres = Repo::getAll();
+        $view = new View("src/views/event");
+        $view->render('index.php', ['events' => $nombres]);
+    }
+
+    function register()
+    {
+        if (isset($_POST['nombre_sms'])) {
+            $event = new Event ($_POST['nombre'],
+                $_POST['nombre_sms'],
+                $_POST['imagen'],
+                $_POST['lugar'],
+                $_POST['fecha'],
+                $_POST['mostrar_comprobar_pulsera'],
+                $_POST['registro_previo'],
+                $_POST['registro_email'],
+                $_POST['registro_telefono']
+            /*$_POST['id_cliente'],
+            $_POST['id_lugar_fb'],
+            $_POST['facebook_pagina'],
+            $_POST['url']*/
+            );
+            $event->save();
+        } else {
+            $event = new Event();
+        }
+        $view = new View("src/views/event");
+        $view->render('register.php', ['event' => $event]);
+    }
+
+    function delete()
+    {
+        if (isset($_POST['nombre_sms'])) {
+            $nombre_sms = $_POST['nombre_sms'];
+            $event = new Event();
+            $event = Repo::findByNombre_sms($nombre_sms);
+            $event->delete();
+        }
+        $event = new Event();
+        $view = new View("src/views/event");
+        $view->render('delete.php', ['event' => $event]);
+    }
+}
