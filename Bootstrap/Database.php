3169bbee3012b2d71908a425730fe18011f752e1
LorenzoMarti <lorenzomartisegura@gmail.com>
2017-04-07 13:49:35 +0200

    Add files via upload
    Modificado el database de bootstrap para coger la info limpia del config/config.php

--- Bootstrap/Database.php
+++ Bootstrap/Database.php
@@ -10,9 +10,16 @@ class Database
  private static function connect()
  {
  	try {
- 	
-       $db = new PDO ('mysql:host=localhost;dbname=casfid', "root", "");
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 		$config = require_once(__DIR__.'/../config/config.php');
+ 		
+    	$db = new PDO (
+       		"mysql:host={$config['db']['host']};
+			dbname={$config['db']['dbname']};
+			charset={$config['db']['charset']}",
+			$config['db']['user'],
+			$config['db']['password']
+		);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
          return $db;
 
  } catch (PDOException $e) {
